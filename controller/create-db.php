1f2ee11cb4862967dce78f2e14c482ca1700b3f0
Maxwell Karp <maxwell.karp@yahoo.com>
2014-10-29 11:28:08 -0700

    finished up to video 13

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,12 +1,12 @@
 <?php
 
-	require_once(__DIR__ . "/../model/database.php");
+	require_once(__DIR__ . "/../model/database.php");  //connects this page to the 
 
-	$connection = new mysqli($host, $username, $password);
+	$connection = new mysqli($host, $username, $password); //helps connect to database.php variables by putting them in an object.  this opens the connection
 
-	if($connection->connect_error){
-		die("Error: "  . $connection->connect_error);
-}
+	if($connection->connect_error){ //runs if there is no connecton to database.php and hte variables aren't getting read
+		die("Error: "  . $connection->connect_error); //eschoes that there is an error
+	}
 	
 
 	$exists = $connection->select_db($database); //try to access a database that exist on the mysql server
@@ -17,13 +17,13 @@
 																	//creates a query that creates a connection to my server
 		if($query){ //checks whether $query was true or not
 
-			echo "successfully created database: " . $database; 
+			echo "successfully created database: " . $database; //echoes this
 		}
 
 	}
 
 	else{ //runs when database has already been created
-		echo "Database already exists";
+		echo "Database already exists"; //echoes this
 	}
 
-	$connection->close();
+	$connection->close(); //this closes the connection
--- model/database.php
+++ model/database.php
@@ -1,6 +1,6 @@
 <?php
 
-	$host = "localhost"; //a variable.  Variables store stuff
-	$username = "root"; //another variable
-	$password = "root"; //is this my third variable?
-	$database = "blog_db"; //Then this would be my fourth
+	$host = "localhost"; //a variable that stores the string "localhost"
+	$username = "root"; //a variable that stores the string "root"
+	$password = "root"; //a variable that stores the string "root"
+	$database = "blog_db"; //a variable that stores the string "root"
