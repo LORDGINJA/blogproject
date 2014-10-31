e7e5c713fa0753336fc18abada6b09a8245c6e01
Maxwell Karp <maxwell.karp@yahoo.com>
2014-10-31 11:13:35 -0700

    video 15

--- controller/create-db.php
+++ controller/create-db.php
@@ -17,19 +17,30 @@
 																	//creates a query that creates a connection to my server
 		if($query){ //checks whether $query was true or not
 
-			echo "successfully created database: " . $database; //echoes this
+			echo "<p>successfully created database: " . $database . "</p>"; //echoes this
 		}
 
 	}
 
 	else{ //runs when database has already been created
-		echo "Database already exists"; //echoes this
+		echo "<p>Database already exists</p>"; //echoes this
 	}
 
 	$query = $connection->query("CREATE TABLE posts ("
 			. "id int(11) NOT NULL AUTO_INCREMENT, " //creates an id for each blogpost.  id cannot be null.  increments id integers
-			. "title varchar (255) NOT NULL" //stores title of blogpost.  can have up to 255 characters.  must have a title
+			. "title varchar (255) NOT NULL, " //stores title of blogpost.  can have up to 255 characters.  must have a title
 			. "post text NOT NULL, " //posts can't be empty
-			. "PRIMARY KEY (id)"); //sets primary key for table.  the way tables are connected to each other
+			. "PRIMARY KEY (id))"); //sets primary key for table.  the way tables are connected to each other
+
+
+	if($query){  //runs if the table is working
+    echo "<p>Succesfully create table: posts</p>";
+
+	}
+
+	else{  //says if the table already exists or if there is an error
+		echo " <p>$connection->error</p>";
+	}
+
 
 	$connection->close(); //this closes the connection
