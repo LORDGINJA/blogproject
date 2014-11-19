1f8912b7d11b22c96ff7e66560499d122944ea58
Maxwell Karp <maxwell.karp@yahoo.com>
2014-11-18 19:55:58 -0800

    Date and Time getting better

--- controller/create-post.php
+++ controller/create-post.php
@@ -7,13 +7,14 @@
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);  //stores title and makes it harder to hack
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);	//stores post and makes it harder to hack
 	$date = new DateTime('today');
+	$time = new DateTime('now');
 
-	$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'"); //stores title and post into database
+	$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post' "); //stores title and post into database
 
 	if ($query) {	//runs if the query was stored
 		echo "<p>Successfully inserted post: $title</p>";
+		echo "Posted on: " . $date->format('M/D' . ' ' . 'd/Y') . " at " . $time->format('G:i');
 		echo "<p><img src='http://www.growhack.com/wp-content/uploads/2013/11/Screen-Shot-2013-11-01-at-3.07.41-PM.png'></p>";
-		echo "Posted on: " . $date->format('m/d/y');
 
 	}
 
--- model/database.php
+++ model/database.php
@@ -1,27 +1,39 @@
 <?php
-	class Database{  //a class is an object that can be used whereever it is called
-		private $connection;  //the "private" before the variable makes it so that these variables can only be used here
-		private $host;  //global variables
+	//a class is an object that can be used whereever it is called
+	class Database{  
+		//the "private" before the variable makes it so that these variables can only be used here
+		private $connection;  
+		//global variables
+		private $host;  
 		private $username;
 		private $password;
 		private $databse;
 		
-		public function __construct($host, $username, $password, $databse){  //allows me to use objects of the Databse class.        Defines class. Parameters allow us to use the global variables for this function
-			$this->host = $host;  //accesses the global variable $host
-			$this->username = $username;  //these are local variables
-			$this->password = $password;  //they are deleted when this function is finished.
+		//allows me to use objects of the Databse class. Defines class. Parameters allow us to use the global variables for this function
+		public function __construct($host, $username, $password, $databse){  
+			//accesses the global variable $host
+			$this->host = $host;  
+			//these are local variables
+			$this->username = $username;  
+			//they are deleted when this function is finished.
+			$this->password = $password;  
 			$this->database = $database;
 		}
-
-		public function openConnection(){  //used to hold repetitive code.  specifically the connection opener
-			$this->connection = new mysqli($this->host, $this->username, $this->password); //accesses the local variables from the construct function
-
-			if($this->connection->connect_error){ //runs if there is no connecton to database.php and hte variables aren't getting read
-				die("Error: "  . $connection->connect_error); //eschoes that there is an error
+		
+		//used to hold repetitive code.  specifically the connection opener
+		public function openConnection(){  
+			//accesses the local variables from the construct function
+			$this->connection = new mysqli($this->host, $this->username, $this->password); 
+			//runs if there is no connecton to database.php and hte variables aren't getting read
+			if($this->connection->connect_error){ 
+				//eschoes that there is an error
+				die("Error: "  . $connection->connect_error); 
 			}
 
 		}
-		public function closeConnection(){  //input the code into the function and insert the function name where ever the code should go
+
+		//input the code into the function and insert the function name where ever the code should go
+		public function closeConnection(){  
 			if(isset($this->connection)){
 				$this->connection->close();
 			}
