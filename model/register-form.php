32f00a6b66da5be368cd559738faa24f5b237800
Maxwell Karp <maxwell.karp@yahoo.com>
2014-12-02 21:01:08 -0800

    working on media queries

---
+++ model/register-form.php
@@ -0,0 +1,21 @@
+<form>
+	<div>
+		<!-- input box for user's email -->
+		<label for="email">Email:</label>
+		<input type="text" name="email"/>
+	</div>
+	<div>
+		<!-- input box for user's username  -->
+		<label for="username">Username:</label>
+		<input type="text" name="username" />
+	</div>
+	<div>
+		<!-- input box for user's password -->
+		<label for="password">Password:</label>
+		<input type="password" name="password"/>
+	</div>
+	<div>
+		<!-- button to submit previous three input values -->
+		<button type="submit">Submit</button>
+	</div>
+</form>
