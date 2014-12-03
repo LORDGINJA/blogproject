d05b5af0e8f6296b4963dc8dcd65a4b554002a44
Maxwell Karp <maxwell.karp@yahoo.com>
2014-12-02 21:03:54 -0800

    Revert "working on media queries"
    This reverts commit 32f00a6b66da5be368cd559738faa24f5b237800.

--- view/header.php
+++ view/header.php
@@ -7,8 +7,6 @@
 			<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
 			<link rel="stylesheet" type="text/css" href="css/boostrap.css">
 			<meta charset="UTF-8">
-			<meta name="viewport" content="width=device-width">
-			<meta name="viewport" content="width=320">
 			<title>Blogs</title>
 		</head>
 		
