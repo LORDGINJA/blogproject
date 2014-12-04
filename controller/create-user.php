27252a9ce02c83aaf01d59ea0b4b601e0391a820
Maxwell Karp <maxwell.karp@yahoo.com>
2014-12-04 11:10:52 -0800

    video 39, creating the register page

---
+++ controller/create-user.php
@@ -0,0 +1,3 @@
+<?php
+	//tells me if the user has been created
+	echo "successfully created user";
--- view/register-form.php
+++ view/register-form.php
@@ -1,17 +1,23 @@
-<form>
+<?php
+	require_once(__DIR__ ."/../model/config.php");
+?>
+
+<h1>REGISTER</h1>
+<!-- goes to create-user page and displays whatevers there -->
+<form method="post" action="<?php echo $path . "controller/create-user.php";?> ">
 	<div>
 		<!-- input box for user's email -->
-		<label for="email">Email:</label>
+		<label for="email">Email: </label>
 		<input type="text" name="email"/>
 	</div>
 	<div>
 		<!-- input box for user's username  -->
-		<label for="username">Username:</label>
+		<label for="username">Username: </label>
 		<input type="text" name="username" />
 	</div>
 	<div>
 		<!-- input box for user's password -->
-		<label for="password">Password:</label>
+		<label for="password">Password: </label>
 		<input type="password" name="password"/>
 	</div>
 	<div>
