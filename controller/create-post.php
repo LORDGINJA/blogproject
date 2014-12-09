b7e5a5238d4848cd517ce0be11807da91be0ab58
Maxwell Karp <maxwell.karp@yahoo.com>
2014-12-09 11:30:31 -0800

    looking very nice

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,11 +1,23 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="../css/post.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+	<link rel="stylesheet" type="text/css" href="css/boostrap.css">
+	<meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=320">
+	<meta charset="UTF-8">
+</head>
+<body id="viewbody">
+
+
+
 
 <?php
 	//gives access to variables listed in $connection
 	require_once(__DIR__ . "/../model/config.php"); 
-
 	
 
-	//stores title and makes it harder to hack
+	//stores title and makes it harder to hack;
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);  
 	//stores post and makes it harder to hack
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);	
@@ -18,14 +30,24 @@
 	
 	//runs if the query was stored
 	if ($query) {	
-		//echoes out that the title was successfully inserted and the name of the title
-		echo "<p>$title</p>";
 
-		echo "<p>$post(10)</p>";
+		echo "<div id='viewpost'>";
+		//echoes out that the title was successfully inserted and the name of the title
+		echo "<h1>$title</h1>";
+
+		
 		//echoes out the date and time of when the post was submitted
 		echo "Posted on: " . $date->format('M/D' . ' ' . 'd/Y') . " at " . $time->format('g:i');
 
+		echo "<p>$post</p>";
+		echo "<br>";
+		echo "<br>";
+		//echo $path . 'home.php';
+		echo "<button type='submit' class='btn btn-primary btn-lg' class='headingbutton' data-toggle='modal' data-target='#myModal'>";
+		echo "<a href='/blogproject/home.php'>Home</a>";
+		echo "</button>";
 
+		echo "</div>";
 
 	}
 	
@@ -34,6 +56,8 @@
 		echo "<p>" . $_SESSION["connection"]->error . "</p>";
 	}
 
+?>
 
 
-
+</body>
+</html>
