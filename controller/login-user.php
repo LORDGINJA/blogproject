d54edb8f23e0a4dd59fb8dd0bc8fa5a40352958d
Maxwell Karp <maxwell.karp@yahoo.com>
2014-12-09 18:30:31 -0800

    video 47. unknown error

--- controller/login-user.php
+++ controller/login-user.php
@@ -1,3 +1,13 @@
 <?php 
+	//gives access to database
+	require_once(__DIR__ . "/../model/config.php");
+	//stores username and filters input
+	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+	//stores username and filters input
+	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+	//selects proper user from database
+	$query = $_SESSION["connection"]->query("SELECT salt, password FROM username = '$username' ");
 
-echo "Successfully created login-form";
+	if(!$query){
+		echo "<p>" . $_SESSION["connection"]->error . "</p>";
+	}
