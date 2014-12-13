ce4b54b586f954b2c27949e050f22c1ba69e31ca
Maxwell Karp <maxwell.karp@yahoo.com>
2014-12-13 10:03:16 -0800

    made the create-user and register page look nice

--- view/register-form.php
+++ view/register-form.php
@@ -4,18 +4,18 @@
 
 <h1>REGISTER</h1>
 <!-- goes to create-user page and displays whatevers there -->   <!-- used to create users -->
-<form method="post" action="<?php echo $path . "controller/create-user.php";?> ">
-	<div>
+<form class="register" method="post" action="<?php echo $path . "controller/create-user.php";?> ">
+	<div id="email">
 		<!-- input box for user's email -->
 		<label for="email">Email: </label>
 		<input type="text" name="email"/>
 	</div>
-	<div>
+	<div id="username">
 		<!-- input box for user's username  -->
 		<label for="username">Username: </label>
 		<input type="text" name="username" />
 	</div>
-	<div>
+	<div id="password">
 		<!-- input box for user's password -->
 		<label for="password">Password: </label>
 		<input type="password" name="password"/>
@@ -25,3 +25,7 @@
 		<button type="submit">Submit</button>
 	</div>
 </form>
+
+<button type="button" class="btn btn-default btn-lg">
+			<a href="<?php echo $path . "post.php"?>"> Blog Post form </a>
+		</button>
